0427183bec4ed4747f48d30ead6981d397db0090
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2023-07-27 23:17:12 -0700

    hamburger nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import Login from './Login';
+import { useState } from 'react';
 import TopTracks from './TopTracks';
 import Nav from './Nav';
 
 const code = new URLSearchParams(window.location.search).get('code');
 
+const AUTH_URL =
+    "https://accounts.spotify.com/authorize?client_id=9bc2ed28c5124518a2b45d4d3d514721&response_type=code&redirect_uri=http://localhost:3000&scope=user-read-private%20user-top-read"
 
-function App() {
+export default function App() {
     console.log(`App`);
 
     function randomBackground() {
@@ -19,8 +21,11 @@ function App() {
         <>
             <div className={`-z-10 ${randomBackground(3)} duration-1000 h-screen w-full fixed top-0 opacity-40 ${code ? ' brightness-100 ' : ' brightness-75 '}`}></div>
             <div className='relative z-20'>
-                {true ? <Nav /> : null}
-                {code ? <TopTracks code={code} /> : <Login />}
+
+                {code ? <Nav code={true} /> : <Nav code={false} />}
+
+                {code ? <TopTracks code={code} /> : <Login /> }
+
                 {code ?
                     <div className='flex-col text-center pb-7 pt-5'>
                         <hr className='my-7 h-0.5 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-950 to-transparent opacity-25 dark:opacity-100 w-1/2 m-auto' />
@@ -35,5 +40,45 @@ function App() {
     )
 }
 
+function Login() {
+    return (
+        <section className='-z-10 absolute flex h-screen w-screen top-0'>
+            <div className='z-30 m-auto font-rale rounded-3xl h-96 w-80 bg-blue-950 text-gray-200 p-7 flex flex-col items-center justify-around shadow-2xl shadow-black'>
+                <h1 className='text-3xl font-mont'>Spotify Radar</h1>
+                <div className='flex flex-col items-center'>
+                    <h4 className='text-center'>Your personal Spotify listening habits all in one
+                        digestable chart.
+                    </h4>
+                    <h4 className=''>Login below to get started</h4>
+                </div>
+                <a href={AUTH_URL} className='bg-green-500 hover:bg-blue-950 hover:text-white hover:border-green-500 border-2 border-blue-950 text-black font-extrabold py-3 px-6 w-fit relative rounded-xl'>
+                    Login with Spotify
+                </a>
+                <div className='items-center flex flex-col text-sm'>
+                    <p className=''>Built by Marc Baeuerle</p>
+                    <p className=''>Project Hosted on <a className='underline duration-200 hover:text-green-500'
+                        href='https://github.com/MarcBaeuerle/Spotify-Radar'>Github</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
 
-export default App;
+function About() {
+    console.log(`ABOUT`);
+    return (
+                <div className='z-50 bg-purple-600 h-20 w-20'> 
+                    About section 
+                </div>
+    )
+}
+
+function PrivacyPolicy() {
+    return (
+                <div> 
+                    Privacy Policy 
+                </div>
+    )
+}
+
--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,21 +1,61 @@
-import React from "react"
+import React, { useState } from "react"
 
-export default function Nav() {
+export default function Nav({code}) {
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setOpenMenu(!openMenu)
+    }
+
+    const logOutButton = (
+                    <button onClick={() => window.location.reload(false)} className='transform duration-300 p-3 hover:-translate-y-1'>
+                        Log out
+                    </button >
+    );
+
+    const homeButton = (
+                    <button className='transform duration-300 p-3 hover:-translate-y-1'>
+                        Home
+                    </button >
+    )
+
+    const aboutButton = (
+        <button onClick={toggleMenu} className='transform duration-300 p-3 hover:-translate-y-1'>
+            About
+        </button >
+
+    )
+
+    const privacyPolicyButton = (
+                <button onClick={toggleMenu} className='h-fulltransform duration-300 p-3 hover:-translate-y-1'>
+                    Privacy Policy
+                </button >
+    )
+
     return (
         <>
-            <nav className='z-50 text-blue-950 flex justify-between p-7 font-rale font-bold'>
-                <div></div>
-                <div className="flex gap-3 sm:gap-4 md:gap-5 text-xl">
-                    <button className='h-fulltransform duration-300 p-3 hover:-translate-y-1'>
-                        Privacy Policy
-                    </button >
-                    <div className="bg-black h-4/5 w-0.5 translate-y-1"> </div>
-                    <button className='transform duration-300 p-3 hover:-translate-y-1'>
-                        About
-                    </button >
+            <div className={`${openMenu ? ' opacity-100 ' : ' opacity-0 '} duration-500 ease-in-out flex flex-col z-0 sm:hidden text-blue-950 font-rale font-bold text-xl absolute w-full h-fit bg-red-100 p-7 shadow-xl`}>
+                {code ?  logOutButton : homeButton }
+                {aboutButton}
+                {privacyPolicyButton}
+            </div>
+            <nav className='z-50 text-blue-950 flex mr-0 ml-auto p-7 font-rale font-bold relative w-fit'>
+                <div onClick={toggleMenu} className="flex flex-col gap-2 p-3 sm:hidden align-middle cursor-pointer">
+                    <span className="bg-black h-1 w-10 rounded-full block"></span>
+                    <span className="bg-black h-1 w-10 rounded-full block"></span>
+                    <span className="bg-black h-1 w-10 rounded-full block"></span>
+                </div>
+
+                <div className="hidden sm:flex gap-3 sm:gap-4 md:gap-5 text-xl">
+                    {code ?  logOutButton : homeButton }
+                    <div className="bg-black h-4/5 w-0.5 translate-y-1"></div>
+                    {aboutButton}
+                    <div className="bg-black h-4/5 w-0.5 translate-y-1"></div>
+                    {privacyPolicyButton}
                 </div>
             </nav>
         </>
     )
 
 }
+
